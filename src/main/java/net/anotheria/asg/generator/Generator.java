e379fa7bb460399799d2c490bf182479b70f4f9a
HELLnyk <yurock.ua@gmail.com>
2020-03-13 11:15:48 +0200

    changed comment version of AnoSiteGenerator and copyright rule

--- src/main/java/net/anotheria/asg/generator/Generator.java
+++ src/main/java/net/anotheria/asg/generator/Generator.java
@@ -200,7 +200,7 @@ public class Generator {
      * @return a {@link java.lang.String} object.
      */
     public static String getVersionString(){
-        return "2.6.3";
+        return "2.6.10";
     }
 
     /**
